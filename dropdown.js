685c820ae449f5cb31150834347df050fe1bae4e
saneenkp <saneenkp2000@gmail.com>
2020-12-15 01:10:55 +0530

    drop down for login created

--- dropdown.js
+++ dropdown.js
@@ -1,4 +1,4 @@
-var path_empty =
+/*var path_empty =
   '<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z"/><path fill-rule="evenodd" d = "M8 1.918l-.797.161A4.00 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />';
 var path_fill =
   '<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>';
@@ -26,3 +26,28 @@ function collapse() {
 function getNoOfEvents() {
   return document.getElementsByClassName("event-panel").length;
 }
+*/
+
+var login = document.getElementById("login");
+
+var drop_down = document.querySelector(".login-options");
+var drop_down_status = false;
+var admin_login = document.getElementById("admin-login");
+var org_login = document.getElementById("Organization-login");
+
+admin_login.addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+
+org_login.addEventListener("click", () => {});
+login.addEventListener("click", () => {
+  console.log(drop_down);
+  if (drop_down_status) {
+    drop_down.style.display = "none";
+
+    drop_down_status = false;
+  } else {
+    drop_down.style.display = "block";
+    drop_down_status = true;
+  }
+});
